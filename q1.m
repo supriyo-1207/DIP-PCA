e43a818e311615828ad44f3fc011deabbba12dd3
Supriyo Maity <supriyomaity990@gmail.com>
2024-10-29 19:23:09 +0530

    Add files via upload

---
+++ q1.m
@@ -0,0 +1,10 @@
+img = imread('vk.jpg');
+gray_img = im2gray(img);
+levels = [128, 64, 32, 16, 8];
+figure;
+for i = 1:length(levels)
+    subplot(1,5,i);
+    imshow(uint8(floor(double(gray_img) / 256 * levels(i)) * (256 / levels(i))));
+    title([num2str(levels(i)) ' gray levels']);
+
+end
---
+++ q2.m
@@ -0,0 +1,10 @@
+	img = imread('vk.jpg');
+	light = img + 50;
+	dark = img - 50;
+	contrast_manupulate = imadjust(img, stretchlim(img), []);
+	negative = 255 - img;
+	figure;
+	subplot(1, 4, 1), imshow(light), title('Increased ');
+	subplot(1, 4, 2), imshow(dark), title('Decreased ');
+	subplot(1, 4, 3), imshow(contrast_manupulate), title('Contrast Manipulated');
+	subplot(1, 4, 4), imshow(negative), title('Negative ');
